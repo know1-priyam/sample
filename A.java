ec827a335a6b46340b07e50d9f446e7ee65ffbc9
Priyam <spriyam522@gmail.com>
2022-11-10 00:20:53 +0530

    First commit

---
+++ A.java
@@ -0,0 +1,14 @@
+class A{
+
+A(int a){
+
+ System.out.println("A constructor");
+}
+
+  public static void main(String[] args){
+   System.out.println("Hello wORLD");
+
+
+     }
+
+}
---
+++ B.java
@@ -0,0 +1,14 @@
+class B extends A{
+
+B(){
+ super(1);
+ System.out.println("B's Constructor");
+
+}
+
+public static void main(String[] args){
+
+B b1=new B();
+
+   }
+}
